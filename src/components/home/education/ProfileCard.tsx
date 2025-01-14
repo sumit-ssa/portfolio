a0a1f3c836dc7b52d06744c6e9736474af8b0a15
sumit-ssa <sumit.socialseller@gmail.com>
2025-01-14 11:02:52 +0530

    profile card updated

--- src/components/home/education/ProfileCard.tsx
+++ src/components/home/education/ProfileCard.tsx
@@ -5,7 +5,11 @@ import { ProfileProps } from "@/types";
 const ProfileCard = ({ data }: { data: ProfileProps }) => {
   //   console.log(data);
   return (
-    <div className="w-full flex flex-col xl:flex-row items-start justify-between bg-black border border-gray-500 rounded-lg px-6 py-3">
+    <div
+      className="w-full flex flex-col xl:flex-row items-start justify-between bg-black border border-gray-500 rounded-lg px-6 py-3"
+      data-aos="fade-up"
+      data-aos-delay="300"
+    >
       <div className="w-full flex items-center gap-4">
         <div className="flex-shrink-0">
           <Image
@@ -14,62 +18,22 @@ const ProfileCard = ({ data }: { data: ProfileProps }) => {
             width={40}
             height={40}
             className="rounded-full object-contain xl:w-[60px] xl:h-[60px]"
-            data-aos="fade-up"
-            data-aos-delay="200"
           />
         </div>
         <div className="flex flex-col">
-          <h2
-            className="text-lg font-semibold text-gray-300"
-            data-aos="fade-up"
-            data-aos-delay="200"
-          >
-            {data?.name}
-          </h2>
-          <p
-            className="text-base text-gray-400 font-medium"
-            data-aos="fade-up"
-            data-aos-delay="300"
-          >
-            {data?.course}
-          </p>
-          <p
-            className="text-sm text-gray-500"
-            data-aos="fade-up"
-            data-aos-delay="400"
-          >
-            {data?.location}
-          </p>
+          <h2 className="text-lg font-semibold text-gray-300">{data?.name}</h2>
+          <p className="text-base text-gray-400 font-medium">{data?.course}</p>
+          <p className="text-sm text-gray-500">{data?.location}</p>
         </div>
       </div>
       {data?.showRightSide && (
         <div className="w-full flex flex-col items-end mt-2 xl:mt-0">
           <div className=" flex items-center gap-2">
-            <h2
-              className="text-sm text-gray-300"
-              data-aos="fade-up"
-              data-aos-delay="100"
-            >
-              {data?.startDate}
-            </h2>
-            <span data-aos="fade-up" data-aos-delay="150">
-              -
-            </span>
-            <h2
-              className="text-sm text-gray-300"
-              data-aos="fade-up"
-              data-aos-delay="200"
-            >
-              {data?.endDate}
-            </h2>
+            <h2 className="text-sm text-gray-300">{data?.startDate}</h2>
+            <span>-</span>
+            <h2 className="text-sm text-gray-300">{data?.endDate}</h2>
           </div>
-          <h2
-            className="text-sm text-gray-300"
-            data-aos="fade-up"
-            data-aos-delay="300"
-          >
-            {data?.duration}
-          </h2>
+          <h2 className="text-sm text-gray-300">{data?.duration}</h2>
         </div>
       )}
     </div>
